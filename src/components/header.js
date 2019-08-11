c0dabef333716abef6872f2e92a7b64b72f48572
Donald Dobbin <djd3dobbin@gmail.com>
2019-08-11 15:10:57 -0400

    landing page almost done, still missing something

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,36 @@ import { Link } from "gatsby"
 import "./header.css"
 
 export default () => (
-  <ul className="header__navbar">
-    <li>
-      <Link className="navbar__item" to="/">
-        Home
-      </Link>
-    </li>
-    <li>
-      <Link className="navbar__item" to="/about/">
-        About Us
-      </Link>
-    </li>
-    <li>
-      <Link className="navbar__item" to="/courses/">
-        Classes
-      </Link>
-    </li>
-    <li>
-      <Link className="navbar__item" to="/contact/">
-        Contact Us
-      </Link>
-    </li>
-  </ul>
+  <div className="header__container">
+    <div className="header__icon__container">
+      <span className="header__icon--torii">
+        <i class="fas fa-torii-gate fa-2x"></i>
+      </span>
+      <span className="header__icon--fist">
+        <i class="fas fa-fist-raised"></i>
+      </span>
+    </div>
+    <ul className="header__navbar">
+      <li>
+        <Link className="navbar__item" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="navbar__item" to="/about/">
+          About Us
+        </Link>
+      </li>
+      <li>
+        <Link className="navbar__item" to="/courses/">
+          Classes
+        </Link>
+      </li>
+      <li>
+        <Link className="navbar__item" to="/contact/">
+          Contact Us
+        </Link>
+      </li>
+    </ul>
+  </div>
 )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,28 @@
 import React from "react"
-import "./home.css";
+import Button from '@material-ui/core/Button';
+import "./home.css"
 import Layout from "../components/layout"
 export default () => (
   <Layout>
-    <div className="home__container">
+    <div className="home__intro">
       <h1 className="home__title">East Coast Karate</h1>
       <p className="home__description">
         What do I like to do? Lots of course but definitely enjoy building
-        websites. What do I like to do? Lots of course but definitely enjoy building
-        websites. What do I like to do? Lots of course but definitely enjoy building
-        websites.
+        websites. What do I like to do? Lots of course but definitely enjoy
+        building websites. What do I like to do? Lots of course but definitely
+        enjoy building websites.
       </p>
+      <Button className="home__button" variant="contained" size="large">Try for Free</Button>
+    </div>
+    <img className="home__image" src="/images/karate-sil.png" />
+    <div className="home__image__text__container">
+      <div className="home__image__text__title">
+        <hr />
+        <p className="home__image__text__item home__image__text__item--title">Karate.</p>
+      </div>
+      <p className="home__image__text__item">Haganah.</p>
+      <p className="home__image__text__item">Krav Maga.</p>
+      <p className="home__image__text__item">Kickboxing.</p>
     </div>
   </Layout>
 )
